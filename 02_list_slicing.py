278ddedda95548dade488c60ef2b37fc893e1bdb
Tahreem-riaz <totehreem101@gmail.com>
2026-09-19 13:50:33 +0500

    Add the program no 4 by creating a list containing the days of week

--- 02_list_slicing.py
+++ 02_list_slicing.py
@@ -57,3 +57,7 @@ print("Every second number:", numbers[::2])
 print("Every third number:", numbers[::3])
 
 # ------------------------------------------
+
+# Q4. Create a list containing the days of the week.
+#
+#     Use slicing with a step to select alternate days.
