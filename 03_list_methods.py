37a80d9de24bd88e3d275db63e7eea708736bee0
Tahreem-riaz <totehreem101@gmail.com>
2026-09-20 12:26:50 +0500

    Add teh program no 1 by creating the list containg three programming languages in list Methods

--- 03_list_methods.py
+++ 03_list_methods.py
@@ -10,3 +10,9 @@ Total Questions: 10
 # ==========================================
 # PART A: ADDING ELEMENTS
 # ==========================================
+
+# Q1. Create a list containing three programming
+#     languages.
+#
+#     Use append() to add two more languages.
+#     Print the updated list.
