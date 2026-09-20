cfe3ec88fd8f3c62e775ce06c00d1a3fcb2f0136
Tahreem-riaz <totehreem101@gmail.com>
2026-09-20 15:03:40 +0500

    Print the recent year award years

--- 04_tuples.py
+++ 04_tuples.py
@@ -110,3 +110,5 @@ print("Alternate periods:", timetable[::2])
 award_years = (2019, 2020, 2021, 2022, 2023, 2024)
 
 recent_years = award_years[-3:]
+
+print("Most recent award years:", recent_years)
