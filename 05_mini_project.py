49422505fb55aebd00d9a3548ec7934a19515773
Tahreem-riaz <totehreem101@gmail.com>
2026-09-20 16:03:48 +0500

    Add the heading of LECTURE 03 - FILE 5: MINI PROJECT and Project: Student Course Tracker Concepts Used: - Lists - List Indexing - List Slicing - List Methods - Tuples - Tuple Indexing - Tuple Slicing - Tuple Methods - Lists vs Tuples

---
+++ 05_mini_project.py
@@ -0,0 +1,18 @@
+"""
+========================================================
+          LECTURE 03 - FILE 5: MINI PROJECT
+========================================================
+Project: Student Course Tracker
+
+Concepts Used:
+- Lists
+- List Indexing
+- List Slicing
+- List Methods
+- Tuples
+- Tuple Indexing
+- Tuple Slicing
+- Tuple Methods
+- Lists vs Tuples
+========================================================
+"""
